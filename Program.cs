b4067a904fe7a2e0b12de5ed6a66a59c01e1debb
MiniEves <evlynks2004@gmail.com>
2026-01-22 11:03:20 -0300

    Atividade C# 2

--- Program.cs
+++ Program.cs
@@ -1,8 +1,18 @@
 ﻿using System;
 using System.Threading;
 
-Pedido p1 = new Pedido(1, "Evelyn")
-Pedido p2 = new
-Pedido p3 = new
+Pedido p1 = new Pedido(1, "Evelyn", 3);
+Pedido p2 = new Pedido(2, "Juan", 5);
+Pedido p3 = new Pedido(3, "Ciane", 2);
+
+Thread t1 = new Thread(p1.Processar); 
+Thread t2 = new Thread(p2.Processar);
+Thread t3 = new Thread(p3.Processar);
+
+t1.Start();
+t2.Start();
+t3.Start();
+
+Console.ReadLine()
 
 //nomedatask.Join();
